303839135aae6aebef51e6bf7e7968aaec00def5
ggurram <gnath.qa@gmail.com>
2020-03-07 23:04:39 -0500

    added by jayasimha

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,6 @@
 Add by jayasimha
+Dat light saving is starting today-jayasimha
+
 Hello Gopi, Thank you for your offer, Sure I will come to US....
 Hello this file contains deployment script
 viswa added line for testing
